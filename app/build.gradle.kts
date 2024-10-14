87c1a69add86002d85552b0dbef2d38e853ada5c
smariba <smaribar@epsg.upv.es>
2024-10-14 15:32:26 +0200

    Commit A

---
+++ app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.smariba_upv.biometric_auth_verif"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.smariba_upv.biometric_auth_verif"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    //Librerias para el uso de la autenticación biométrica
+    implementation("androidx.biometric:biometric:1.2.0")
+    implementation("androidx.biometric:biometric-ktx:1.4.0-alpha02")
+}
